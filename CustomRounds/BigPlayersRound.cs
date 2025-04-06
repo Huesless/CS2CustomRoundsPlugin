c7907b6a27a01630e5f4d5b5e3555b4446853265
HuesLess <nithieuuni@gmail.com>
2025-04-06 17:04:09 +0200

    Added BigPlayersRound, SmallPlayersRound

---
+++ CustomRounds/BigPlayersRound.cs
@@ -0,0 +1,24 @@
+﻿using CounterStrikeSharp.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS2CustomRoundsPlugin
+{
+    public class BigPlayersRound : CustomRound
+    {
+        public override string RoundStartMessage => "BIG MODE";
+        public override string RoundStartDescription => "Everyone is BIG.";
+        public override void RoundStart()
+        {
+            Server.ExecuteCommand("c_entfire player SetScale 2");
+        }
+        public override void RoundEnd()
+        {
+            Server.ExecuteCommand("c_entfire player SetScale 1");
+        }
+
+    }
+}
---
+++ CustomRounds/SmallPlayerRound.cs
@@ -0,0 +1,23 @@
+﻿using CounterStrikeSharp.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS2CustomRoundsPlugin
+{
+    public class SmallPlayerRound : CustomRound
+    {
+        public override string RoundStartMessage => "small mode";
+        public override string RoundStartDescription => "Everyone is small.";
+        public override void RoundStart()
+        {
+            Server.ExecuteCommand("c_entfire player SetScale 0.5");
+        }
+        public override void RoundEnd()
+        {
+            Server.ExecuteCommand("c_entfire player SetScale 1");
+        }
+    }
+}
